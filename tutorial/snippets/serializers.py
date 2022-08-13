6ab78532b440fae50627499ea6c241f40b84a190
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-08-13 16:23:50 +0530

    using hyperlinks

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -5,16 +5,18 @@ from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+    
     class Meta:
         model = Snippet
-        fields = '__all__'
+        fields = ['url', 'id', 'highlight', 'owner', 'title', 'code', 'linenos', 'language', 'style']
 # go to shell and type repr(SnippetSerializer())
 #it lists all the fields as they were when we defined them in the model
 
 
 class UserSerializer(serializers.ModelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(queryset=Snippet.objects.all(), many=True)
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
